6db3aeb2fd9f208f8794a905b5c808ecc57e3a1b
atee86 <jecy3k@gmail.com>
2017-01-29 15:06:04 +0200

    Database Helper

---
+++ DiceGame/Models/Helpers/DataBaseHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiceGame.Models.Helpers
+{
+    public static class DataBaseHandler
+    {
+        public static List<User> GetUsers()
+        {
+            List<User> userslist = new List<User>();
+
+            return userslist;
+        }
+
+        public static bool SetUsers(List<User> userslist)
+        {
+            bool success = false;
+
+            return success;
+        }
+    }
+}
